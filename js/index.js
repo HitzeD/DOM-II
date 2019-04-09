79ee11c97d9e08b3c39bb8afc793778d2fd5c8cf
HitzeD <d-hitze@comcast.net>
2019-04-09 15:24:27 -0500

    periodic save, window alert, and 2 others added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+const btn = document.querySelectorAll('.btn');
+const intro = document.querySelector('.intro');
+const midSec = document.querySelectorAll('.content-section');
+const page = document.querySelector('html');
+
+
+
+window.addEventListener('load', (event) => {
+    alert('There are 9 hidden Event Listeners on this page. Can you find them all?');
+});
+
+intro.addEventListener('click', (event) => {
+    intro.style.textAlign = 'center';
+})
+
+btn[0].addEventListener('click', (event) => {
+    btn[0].style.backgroundColor = 'black';
+})
+
+btn[1].addEventListener('load', (event) => {
+    btn[1].style.textSize = '3rem';
+})
+
+midSec[0].addEventListener('')
+
+
+
+console.log(page);
+
